09f07bd61b8320d67a98a3d14b4f6aa69f19451d
Paulo Piva <pauloedupiva@gmail.com>
2025-12-02 00:32:01 -0300

    UI: menu lateral fixo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,31 @@
 import streamlit as st
-
 from ui import upload, calculo, pre_oc, oc_oficial, historico, alocacao
 
 st.set_page_config(page_title="Alivvia Gestão", layout="wide")
 
-MENU = {
-    "Upload": upload,
-    "Cálculo": calculo,
-    "Pré-OC": pre_oc,
-    "OC Oficial": oc_oficial,
-    "Histórico": historico,
-    "Alocação": alocacao
-}
-
+# --- MENU FIXO NA ESQUERDA ---
 st.sidebar.title("Menu")
-escolha = st.sidebar.selectbox(" ", list(MENU.keys()))
+pagina = st.sidebar.radio(
+    "Navegação",
+    ["Upload", "Cálculo", "Pré-OC", "OC Oficial", "Histórico", "Alocação"],
+    index=0
+)
 
-MENU[escolha].app()
+# --- ROTEAMENTO DAS PÁGINAS ---
+if pagina == "Upload":
+    upload.app()
+
+elif pagina == "Cálculo":
+    calculo.app()
+
+elif pagina == "Pré-OC":
+    pre_oc.app()
+
+elif pagina == "OC Oficial":
+    oc_oficial.app()
+
+elif pagina == "Histórico":
+    historico.app()
+
+elif pagina == "Alocação":
+    alocacao.app()
